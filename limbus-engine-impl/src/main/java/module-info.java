74c5a3ef337ccf11230628c4ae58f829667a9e0e
Christopher Schuette <christopher.schuette@remondis.de>
2019-10-30 17:42:39 +0100

    OpenJDK 11
    - Dependency updates.
    - Maven Update
    - Package structure reorganized to avoid split packages
    - Compile Source and Target set to 11

--- limbus-engine-impl/src/main/java/module-info.java
+++ limbus-engine-impl/src/main/java/module-info.java
@@ -1,2 +1,6 @@
 module com.remondis.limbus.engine.implementation {
+  requires com.remondis.limbus.api;
+  requires com.remondis.limbus.engine.interfaces;
+  requires com.remondis.limbus.system;
+  requires com.remondis.limbus.utils;
 }
